40af4c6c4da7a03912276abdfd7b1401218c2d22
danielCnichols <herrnichols@gmail.com>
2019-09-18 15:51:24 -0400

    A good review

---
+++ index.js
@@ -0,0 +1,57 @@
+/* eslint-disable indent */
+
+
+function generateResults(resJson) {
+	let results = [];
+	if (resJson.length !== 0) {
+		results = resJson.map(repo => {
+		return `
+				<h3>Name: ${repo.name}</h3>
+			<p>URL:<a href="${repo.url}">${repo.url}</a></p>`;
+	});
+} else {
+	results = '<h2>Sorry, no results found</h2>';
+}
+	displayResults(results);
+}
+
+
+function displayResults(results) {
+	$('.results').empty();
+	$('.results').append(results);
+}
+
+function getResults(searchTerm) {
+  const baseURL = `https://api.github.com/users/${searchTerm}/repos` ;
+
+
+  fetch(baseURL)
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      throw new Error (res.statusText);
+    })
+    .then(resJson => generateResults(resJson))
+    .catch(err => {
+      $('#js-error').text(`something went wrong: ${err.message}`);
+    });
+}
+
+function handleSearch() {
+  $('#form').submit(function(event) {
+    event.preventDefault();
+		let searchTerm = $('.form_searchbar').val();
+		$('#form').trigger('reset');
+    getResults(searchTerm);
+  });
+}
+
+
+
+
+function main() {
+  handleSearch();
+}
+
+$(main);
